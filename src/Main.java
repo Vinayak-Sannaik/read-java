4fcc2e36f99293124d8c168c1fcd7c9f2df408e6
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2025-05-16 07:27:33 +0530

    first greater number

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("HI");
-        System.out.println("Vinayak");
-        System.out.println("Vinayak sannnaik");
-        System.out.println("Vinayak sannnaik 1");
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String args[]) {
+        int a = 12, b = 2, c = 9;
+        if(a > b && a > c){
+            System.out.println(a + " is greater");
+        }else if(c > a && c > b){
+            System.out.println(c + " is greater");
+        }else{
+            System.out.println(b + " is greater");
+        }
     }
 }
